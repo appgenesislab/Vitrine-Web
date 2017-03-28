c125945bc7b065d5083a9200bc479832aaa2ca14
Mario Paulo <mariopaulosoares@gmail.com>
2017-03-28 22:11:02 +0200

    Setup initial project structure

---
+++ src/main/java/com/appgenesislab/VitrineApplication.java
@@ -0,0 +1,12 @@
+package com.appgenesislab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitrineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitrineApplication.class, args);
+	}
+}
